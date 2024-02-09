98987d25cab5047083a83b73cb25d2453fa28d68
Chester <89991461+ChesterJaz@users.noreply.github.com>
2024-02-09 15:01:27 +0800

    testing-api-auth

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose'
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}))
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+const mongo = "mongodb+srv://chester:chester123@cluster0.9g3fmci.mongodb.net/?retryWrites=true&w=majority"
+
+const server = http.createServer(app)
+const port = 8080;
+
+server.listen(port, () =>{
+    console.log(`server listening on http://localhost:${port}/`)
+})
+
+mongoose.Promise = Promise;
+mongoose.connect(mongo)
+mongoose.connection.on('error', (error: Error) => console.log(error))
